16bef00e38ceb9e1481375c16cd0011ae9b5da53
CHIkovil <chikovil3488@gmail.com>
2024-02-05 15:57:43 +0300

    modified data type for parsing and search pattern principle changed

--- src/app.py
+++ src/app.py
@@ -1,5 +1,11 @@
-from src.Parser.img_to_model_parser import ImageToModelParser
+from src.Parser.file_to_model_parser import FileToModelParser
+from src.Parser.file_parser_operations import DataType
 from asyncio import run as aio_run
+from aiofiles import open as aio_open
+from pathlib import Path
+
+
 
 if __name__ == "__main__":
     aio_run(on_example_script())
+
